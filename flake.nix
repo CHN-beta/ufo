8ce10cb6c2fcfab028457abdd963c4ed7251123d
chn <chn@chn.moe>
2023-09-07 23:08:31 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+  outputs = inputs: let pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux; in
+  {
+    devShell.x86_64-linux = pkgs.mkShell.override { stdenv = pkgs.gcc13Stdenv; }
+    {
+      buildInputs = with pkgs; [ yaml-cpp eigen ];
+    };
+  };
+}
