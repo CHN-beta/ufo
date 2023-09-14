fd5719a1e2b4aa269731858a0769f4a16ce717af
chn <chn@chn.moe>
2023-09-15 02:25:46 +0800

    fix: phonopy 输出的已经是周期的结果

--- plot.cpp
+++ plot.cpp
@@ -45,13 +45,13 @@ int main()
   }
 
   std::ofstream os("out.dat");
-  // 31 个不同的 q 点
+  // 13 个不同的 q 点
   // -5 \sim 30 THz, 分辨率为 0.01 THz
   // 先按照三角形进行涂抹, 虽然我觉得更合理的可能应该是正态分布
   // 当高度达到 1 时, 涂抹的宽度达到 0.4 THz
 
   // 构建未涂抹时的数据
-  std::vector<std::vector<double>> original_data(31, std::vector<double>(3501));
+  std::vector<std::vector<double>> original_data(13, std::vector<double>(3501));
   for (auto& data1: original_data)
     for (auto& data2: data1)
       assert(data2 == 0);
@@ -59,14 +59,14 @@ int main()
   for (auto qpoint: output.QPointData)
     for (auto mode: qpoint.ModeData)
       if (qpoint.QPoint[1] < 1e-3 && qpoint.QPoint[2] < 1e-3)
-        original_data.at(static_cast<int>(qpoint.QPoint[0] * 30 + 0.5)).at(static_cast<int>(mode.Frequency / 0.01 + 500 + 0.5)) += mode.Weight;
+        original_data.at(static_cast<int>(qpoint.QPoint[0] * 12 + 0.5)).at(static_cast<int>(mode.Frequency / 0.01 + 500 + 0.5)) += mode.Weight;
 
   // 涂抹
   auto data = original_data;
   for (auto& data1: data)
     data1.assign(data1.size(), 0);
 
-  for (int i = 0; i < 31; i++)
+  for (int i = 0; i < 13; i++)
     for (int j = 0; j < 3501; j++)
     {
       data[i][j] += std::sqrt(original_data[i][j]);
@@ -76,13 +76,13 @@ int main()
     }
 
   // 输出
-  for (int i = 0; i < 31; i++)
-    os << fmt::format("\t{}", static_cast<double>(i) / 30);
+  for (int i = 0; i < 13; i++)
+    os << fmt::format("\t{}", static_cast<double>(i) / 12);
   os << "\n";
   for (int i = 0; i < 3501; i++)
   {
     os << fmt::format("{}", static_cast<double>(i - 500) * 0.01);
-    for (int j = 0; j < 31; j++)
+    for (int j = 0; j < 13; j++)
       os << fmt::format("\t{}", data[j][i]);
     os << "\n";
   }
