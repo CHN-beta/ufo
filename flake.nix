c4d613e2d0352b485c9985b49b42f5989bfc0026
chn <chn@chn.moe>
2023-09-19 13:21:38 +0800

    增加调试模式 修正计算权重时的错误

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     devShell.x86_64-linux = pkgs.mkShell.override { stdenv = pkgs.genericPackages.gcc13Stdenv; }
     {
       buildInputs = with pkgs;
-        [ yaml-cpp eigen fmt (localPackages.concurrencpp.override { stdenv = genericPackages.gcc13Stdenv; }) ];
+        [ yaml-cpp eigen fmt (localPackages.concurrencpp.override { stdenv = genericPackages.gcc13Stdenv; }) highfive ];
       nativeBuildInputs = with pkgs; [ gdb ];
     };
   };
