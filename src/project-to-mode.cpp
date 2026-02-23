27dbe7ad91b6cb2d482ad02038b916ce910589d8
Haonan Chen <chn@chn.moe>
2026-02-23 17:25:53 +0800

    project-to-mode: match atom instead of using plane wave

--- src/project-to-mode.cpp
+++ src/project-to-mode.cpp
@@ -1,14 +1,16 @@
 # include <ufo.hpp>
+# include <execution>
 
 void ufo::project_to_mode(std::string config_file)
 {
   // 将选定的超胞中的某一个 q 点上的每一个模式到反折叠后的某个 q 点的投影，投射到单胞中的对应 q 点的模式上
-  // 与 unfold 的方法类似，只有一个重要的区别：
-  // 组成基矢的平面波的波矢的两个部分中，第二个部分不再需要。因为这个部分的结果已经算出来了，只需要将结果乘上 WeightOnUnfold 即可。
+  // 不使用平面波方法，而是匹配对应的原子然后做内积
   struct Config
   {
-    // 在单胞内取几个平面波的基矢
-    Eigen::Vector3i PrimativeCellBasisNumber;
+    // 容许的原子位置偏差，单位为埃
+    double AtomPositionTolerance;
+    // q 点位置的容许偏差，单位为埃^-1
+    double QpointTolerance;
     // 要选定的超胞中的 q 点，sub qpoint，以及单胞中的 q 点
     std::size_t SuperQpointIndex, SubQpointIndex, PrimativeQpointIndex;
     // 输入输出文件的路径，直接输出 yaml，因为这些数据不再需要后处理
@@ -32,99 +34,113 @@ void ufo::project_to_mode(std::string config_file)
     .Coefficient = std::vector<std::vector<double>>(input.Super.Qpoint[config.SuperQpointIndex].Mode.size(),
       std::vector<double>(input.Primative.Qpoint[config.PrimativeQpointIndex].Mode.size()))
   };
-  if ((output.SubQpoint - output.PrimativeQpoint).norm() > 0.01)
+  if (((output.SubQpoint - output.PrimativeQpoint).transpose() * input.Primative.Cell.inverse().transpose()).norm()
+    > config.QpointTolerance)
     log.error("sub qpoint {} != primative qpoint {}"_f(output.SubQpoint, output.PrimativeQpoint));
 
-  // 基在原胞中的表示
-  auto primative_basis = [&]
+  auto atom_is_near = [&](Eigen::Vector3d atom_in_super, Eigen::Vector3d atom_in_primative) -> bool
   {
-    biu::Logger::Guard log;
-    std::vector<Eigen::VectorXcd> basis((config.PrimativeCellBasisNumber.array() * 2 - 1).prod());
-    for (auto [xyz_of_basis, i_of_basis] : biu::sequence
-      ((-config.PrimativeCellBasisNumber + Eigen::Vector3i::Ones()).eval(), config.PrimativeCellBasisNumber))
-    {
-      // 波矢就是 xyz_of_basis, 单位为单胞的倒格矢
-      // 将它的单位转换成埃^-1
-      auto wavevector =
-        (xyz_of_basis.transpose().cast<double>() * input.Primative.Cell.inverse().transpose()).transpose();
-      // 将原子坐标单位也转换为埃
-      auto atom_position = input.Primative.AtomPosition * input.Primative.Cell;
-      // 计算基矢
-      basis[i_of_basis] = (2i * std::numbers::pi_v<double> * (atom_position * wavevector)).array().exp();
-    }
-    return basis;
-  }();
-  // 基在超胞中的表示
-  auto super_basis = [&]
+    // 如果两个原子经过变换后正好重合，那么应该有这个关系：
+    // (atom_in_super - AtomTranslation) * CellDeformation * CellMultiplier * PrimativeCell
+    //    = atom_in_primative * PrimativeCell
+    // 即:
+    // (atom_in_super - AtomTranslation) * CellDeformation * CellMultiplier - atom_in_primative = 0
+    // 但这里要考虑两个因素：
+    // 首先，两个原子可能会差整数个 PrimativeCell 的格矢，所以需要对上面的结果加或者减一个整数，使得结果落在正负 0.5 范围内
+    // 其次，我们允许一定误差，因此上面的数值乘以 PrimativeCell 的模应该小于 AtomPositionTolerance
+    biu::Logger::Guard log(atom_in_super.transpose(), atom_in_primative.transpose());
+    auto translation = input.Super.AtomTranslation.value_or(std::array{0., 0., 0.})
+      | biu::toEigen<>;
+    auto diff = ((atom_in_super - translation).transpose() * input.Super.CellDeformation
+      * input.Super.CellMultiplier.cast<double>().asDiagonal() - atom_in_primative.transpose()).eval();
+    for (auto i = 0; i < 3; i++) diff[i] -= std::round(diff[i]);
+    log.debug("Diff: {}"_f(diff.transpose()));
+    return (diff * input.Primative.Cell).norm() < config.AtomPositionTolerance;
+  };
+
+  // 匹配原子
+  int number_of_matched_atoms = 0, number_of_unmatched_atoms = 0;
+  auto type_in_super = input.Super.AtomType
+    | std::views::transform([](const auto& pair) { return std::views::repeat(pair.first, pair.second); })
+    | std::views::join
+    | std::ranges::to<std::vector<std::string>>();
+  auto type_in_primative = input.Primative.AtomType
+    | std::views::transform([](const auto& pair) { return std::views::repeat(pair.first, pair.second); })
+    | std::views::join
+    | std::ranges::to<std::vector<std::string>>();
+  std::vector<std::optional<int>> matched_atoms_in_super(input.Super.AtomPosition.rows());
+  for (auto i_of_super_atom : std::views::iota(0, input.Super.AtomPosition.rows()))
   {
-    biu::Logger::Guard log;
-    std::vector<Eigen::VectorXcd> basis((config.PrimativeCellBasisNumber.array() * 2 - 1).prod());
-    auto diff_of_sub_qpoint_by_reciprocal_modified_super_cell = [&]
+    auto result = std::views::iota(0, input.Primative.AtomPosition.rows())
+      | std::views::filter([&](auto i_of_primative_atom)
+        {
+          return type_in_super[i_of_super_atom] == type_in_primative[i_of_primative_atom]
+            && atom_is_near
+              (input.Super.AtomPosition.row(i_of_super_atom), input.Primative.AtomPosition.row(i_of_primative_atom));
+        })
+      | std::ranges::to<std::vector<int>>();
+    if (result.empty())
     {
-      for
+      log.debug("Unmatched atom in super cell: {} {} {}"_f
       (
-        auto [diff_of_sub_qpoint_by_reciprocal_modified_super_cell, i_of_sub_qpoint]
-          : biu::sequence(input.Super.CellMultiplier)
-      )
-        if (i_of_sub_qpoint == config.SubQpointIndex) return diff_of_sub_qpoint_by_reciprocal_modified_super_cell;
-      std::unreachable();
-    }();
-    for (auto [xyz_of_basis, i_of_basis] : biu::sequence
-      ((-config.PrimativeCellBasisNumber + Eigen::Vector3i::Ones()).eval(), config.PrimativeCellBasisNumber))
-    {
-      // 计算波矢, 单位为单胞的倒格矢
-      auto wavevector_by_reciprocal_primative_cell = xyz_of_basis.cast<double>()
-        + input.Super.CellMultiplier.cast<double>().cwiseInverse().asDiagonal()
-        * diff_of_sub_qpoint_by_reciprocal_modified_super_cell.cast<double>();
-      // 将单位转换为埃^-1
-      auto wavevector =
-        (wavevector_by_reciprocal_primative_cell.transpose() * input.Primative.Cell.inverse().transpose()).transpose();
-      // 将原子坐标单位也转换为埃
-      auto atom_translation =
-        (input.Super.AtomTranslation.value_or(std::array{0., 0., 0.}) | biu::toEigen<>)
-        .transpose().replicate(input.Super.AtomPosition.rows(), 1).eval();
-      auto atom_position = (input.Super.AtomPosition - atom_translation)
-        * (input.Super.CellDeformation * input.Super.CellMultiplier.cast<double>().asDiagonal() * input.Primative.Cell);
-      // 计算基矢
-      basis[i_of_basis] = (2i * std::numbers::pi_v<double> * (atom_position * wavevector)).array().exp();
+        i_of_super_atom, type_in_super[i_of_super_atom],
+        input.Super.AtomPosition.row(i_of_super_atom).transpose()
+      ));
+      number_of_unmatched_atoms++;
     }
-    return basis;
-  }();
-  // 将所有模式投影到基矢上
-  auto primative_projection_coefficient =
-    input.Primative.Qpoint[config.PrimativeQpointIndex].Mode
-      | ranges::views::transform([&](const auto& mode)
-        {
-          return primative_basis
-            | ranges::views::transform([&](const auto& basis)
-              { return (basis.transpose().conjugate() * mode.EigenVector).eval() | biu::fromEigen; })
-            | ranges::to_vector
-            | biu::toEigen<>;
-        })
-      | ranges::to_vector;
-  auto super_projection_coefficient =
-    input.Super.Qpoint[config.SuperQpointIndex].Mode
-      | ranges::views::transform([&](const auto& mode)
-        {
-          return super_basis
-            | ranges::views::transform([&](const auto& basis)
-              { return (basis.transpose().conjugate() * mode.EigenVector).eval() | biu::fromEigen; })
-            | ranges::to_vector
-            | biu::toEigen<>;
-        })
-      | ranges::to_vector;
-  // 将它们互相投影，得到结果
-  for (auto [i_of_super_mode, super_mode] : ranges::views::enumerate(super_projection_coefficient))
-  {
-    for (auto [i_of_primative_mode, primative_mode]
-      : ranges::views::enumerate(primative_projection_coefficient))
-      output.Coefficient[i_of_super_mode][i_of_primative_mode] =
-        std::norm((super_mode.transpose().conjugate() * primative_mode).trace());
-    auto sum = ranges::accumulate(output.Coefficient[i_of_super_mode], 0.);
-    for (auto& coefficient : output.Coefficient[i_of_super_mode])
-      coefficient *=
-        input.Super.Qpoint[config.SuperQpointIndex].Mode[i_of_super_mode].WeightOnUnfold[config.SubQpointIndex] / sum;
+    else if (result.size() > 1)
+    {
+      log.error("Multiple matched atoms in primative cell: {} {} {}"_f
+      (
+        i_of_super_atom, type_in_super[i_of_super_atom],
+        input.Super.AtomPosition.row(i_of_super_atom).transpose()
+      ));
+      number_of_unmatched_atoms++;
+    }
+    else
+    {
+      matched_atoms_in_super[i_of_super_atom] = result[0];
+      number_of_matched_atoms++;
+    }
   }
+  log.info("Matched {} atoms, unmatched {} atoms"_f(number_of_matched_atoms, number_of_unmatched_atoms));
+
+  // 对每个模式进行投影
+  std::for_each_n
+  (std::execution::par_unseq,
+    std::views::iota(0).begin(),
+    input.Super.Qpoint[config.SuperQpointIndex].Mode.size(),
+    [&](auto i_of_mode)
+    {
+      auto& super_mode = input.Super.Qpoint[config.SuperQpointIndex].Mode[i_of_mode];
+      for (auto i_of_primative_mode
+        : std::views::iota(0u, input.Primative.Qpoint[config.PrimativeQpointIndex].Mode.size()))
+      {
+        auto& primative_mode = input.Primative.Qpoint[config.PrimativeQpointIndex].Mode[i_of_primative_mode];
+        output.Coefficient[i_of_mode][i_of_primative_mode] = std::norm(*std::ranges::fold_left_first
+        (
+          std::views::iota(0, input.Super.AtomPosition.rows())
+            | std::views::filter([&](auto i_of_super_atom)
+              { return matched_atoms_in_super[i_of_super_atom].has_value(); })
+            | std::views::transform([&](auto i_of_super_atom) -> std::complex<double>
+              {
+                return super_mode.EigenVector.row(i_of_super_atom)
+                  * primative_mode.EigenVector.row(*matched_atoms_in_super[i_of_super_atom]).transpose().conjugate();
+              }),
+          std::plus{}
+        ));
+      }
+      // 将结果进行放缩，使得系数之和为 input.Super.Qpoint[config.SuperQpointIndex].Mode[i_of_mode]
+      //    .WeightOnUnfold[config.SubQpointIndex]
+      double ratio;
+      if (auto sum = *std::ranges::fold_left_first(output.Coefficient[i_of_mode], std::plus{}); sum == 0)
+        ratio = 0;
+      else
+        ratio = input.Super.Qpoint[config.SuperQpointIndex].Mode[i_of_mode].WeightOnUnfold[config.SubQpointIndex] / sum;
+      for (auto& coefficient : output.Coefficient[i_of_mode])
+        coefficient *= ratio;
+    }
+  );
   // 输出
   std::ofstream(config.OutputFile) << YAML::Node(output);
 }
