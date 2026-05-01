621bcb09b28025f7d811f6ff2ae51603f9b9be0d
Haonan Chen <chn@chn.moe>
2026-05-01 23:14:20 +0800

    add github workflow

--- flake.nix
+++ flake.nix
@@ -6,9 +6,10 @@
     packages.x86_64-linux = let inherit (inputs.nixos.packages.x86_64-linux) pkgs; in rec
     {
       default = pkgs.pkgsStatic.localPkgs.buildUfo ./.;
-      release = pkgs.runCommand "x86_64-linux-musl-static.zip" {}
+      release = pkgs.runCommand "release" {}
       ''
-        ${pkgs.zip}/bin/zip -r $out ${default}/bin/ufo
+        mkdir -p $out
+        ${pkgs.zip}/bin/zip -r $out/x86_64-linux-musl-static.zip ${default}/bin/ufo
       '';
     };
   };
